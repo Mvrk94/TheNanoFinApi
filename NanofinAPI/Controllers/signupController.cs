a2b8d6633f21272c6e99cc87764079566533e939
mark.garber94@gmail.com <mark.garber94@gmail.com>
2016-10-05 14:35:16 +0200

    complete reseller sign up.

--- NanofinAPI/Controllers/signupController.cs
+++ NanofinAPI/Controllers/signupController.cs
@@ -2,6 +2,7 @@
 using NanofinAPI.Models.DTOEnvironment;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -19,15 +20,28 @@ namespace NanofinAPI.Controllers
         database_nanofinEntities db = new database_nanofinEntities();
 
         //post user - return true if user created. userType - 11 for consumer & 21 for reseller
-        [HttpPost]
-        [ResponseType(typeof(DTOuser))]
-        public async Task<DTOuser> postUser(string fName, string lName, string userName, string email,string contactNum, string userPass, int userType, string IDnumber )
+        public async Task<DTOuser> postUser(string fName, string lName, string userName, string email,string contactNum, string userPass, int userType, string IDnumber ) 
         {
             user tmp = new user();
             tmp.userFirstName = fName;
             tmp.userLastName = lName;
-            tmp.userName = userName;
-            tmp.userEmail = email;
+            //email is taken return empty user object before changes to db made
+            if (await isUsernameTaken(userName))
+            {
+                tmp.userFirstName = null;
+                tmp.userLastName = null;
+                tmp.userName = "-1";
+                return new DTOuser(tmp);
+            }else{ tmp.userName = userName; }
+            
+            if (await isEmailTaken(email))
+            {
+                tmp.userFirstName = null;
+                tmp.userLastName = null;
+                tmp.userEmail = "-1";
+                return new DTOuser(tmp);
+            }else{ tmp.userEmail = email; }
+
             tmp.userContactNumber = contactNum;
             tmp.userPassword = encryptPass(userPass);
             tmp.userIsActive = true;
@@ -64,12 +78,25 @@ namespace NanofinAPI.Controllers
            return BCrypt.Net.BCrypt.HashPassword(password);
         }
 
-        //post user - return true if user created. userType - 11 for consumer & 21 for reseller
-        [HttpPost]
-        [ResponseType(typeof(DTOconsumer))]
-        public async Task<DTOconsumer> postConsumer(string fName, string lName, string userName, string email, string contactNum, string userPass,  string IDnumber, DateTime DOB, string gender, string maritalStatus, string employmentStatus)
+
+        public async Task<IHttpActionResult> postConsumer(string fName, string lName, string userName, string email, string contactNum, string userPass, string IDnumber, DateTime DOB, string gender, string maritalStatus, string employmentStatus)
         {
             DTOuser newUser = await postUser(fName, lName, userName, email, contactNum, userPass, 11, IDnumber);
+
+            if (newUser.userFirstName == null && newUser.userLastName == null)
+            {
+                if(newUser.userName == "-1")
+                {
+                    return BadRequest("Username taken");
+                }else if (newUser.userEmail == "-1")
+                {
+                    return BadRequest("email taken");
+                }
+                else
+                {
+                    return BadRequest("Invalid user");
+                }
+            }
 
             consumer tmp = new consumer();
             tmp.User_ID = newUser.User_ID;
@@ -83,29 +110,61 @@ namespace NanofinAPI.Controllers
             db.consumers.Add(tmp);
             await db.SaveChangesAsync();
 
-            return new DTOconsumer(tmp);
+            return Ok();
         }
 
         //post user - return true if user created. userType - 11 for consumer & 21 for reseller
-        //[HttpPost]
-        //[ResponseType(typeof(DTOreseller))]
-        //public async Task<DTOreseller> postReseller(string fName, string lName, string userName, string email, string contactNum, string userPass, int userType, string IDnumber, string cardNumber, string cardExpiration, string cardCVV, string nameOnCard, string bankName, DateTime DOB)
-        //{
-        //    DTOuser newUser = await postUser(fName, lName, userName, email, contactNum, userPass, 11, IDnumber);
+        public async Task<IHttpActionResult> postReseller(string fName, string lName, string userName, string email, string contactNum, string userPass, string IDnumber, string cardNumber, string cardExpiration, string cardCVV, string nameOnCard, string bankName, DateTime DOB)
+        {
+            DTOuser newUser = await postUser(fName, lName, userName, email, contactNum, userPass, 21, IDnumber);
 
-        //    reseller tmp = new reseller();
-        //    tmp.User_ID = newUser.User_ID;
-        //    tmp.resellerIsValidated = true;
-        //    //tmp.cardNumber = cardNumber;
-            
+            if (newUser.userFirstName == null && newUser.userLastName == null)
+            {
+                if (newUser.userName == "-1")
+                {
+                    return BadRequest("Username taken");
+                }
+                else if (newUser.userEmail == "-1")
+                {
+                    return BadRequest("email taken");
+                }
+                else
+                {
+                    return BadRequest("Invalid user");
+                }
+            }
 
+            reseller tmp = new reseller();
+            tmp.Reseller_ID = newUser.User_ID;
+            tmp.User_ID = newUser.User_ID;
+            tmp.resellerIsValidated = true;
+            tmp.cardNumber = cardNumber;
+            tmp.cardExpirationMonth_Year = cardExpiration;
+            tmp.cardCVV = cardCVV;
+            tmp.nameOnCard = nameOnCard;
+            tmp.resellerBankName = bankName;
+            tmp.resellerDateOfBirth = DOB;
+            tmp.isSharingLocation = "false";
 
-        //    db.resellers.Add(tmp);
-        //    await db.SaveChangesAsync();
+            tmp.resellerBankBranchName = "";
+            tmp.resellerBankAccountNumber = "";
+            tmp.resellerBankBranchCode = "";
 
+            db.resellers.Add(tmp);
+            db.SaveChanges();
 
-        //    return new DTOreseller(tmp);
-        //}
+            return Ok();
+        }
+
+        public async Task<bool> isEmailTaken(string email)
+        {
+            return await db.users.AnyAsync(l => l.userEmail == email);
+        }
+
+        public async Task<bool> isUsernameTaken(string userName)
+        {
+            return await db.users.AnyAsync(l => l.userName == userName); ;
+        }
 
 
     }
